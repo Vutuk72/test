272a3205c70379819287a63616bdfb0b3a04f778
vcf <v@fk.cz>
2024-08-20 10:35:27 +0200

    init app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connection to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers.js')
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => console.log('Server Started port:3000'))
